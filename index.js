0bc003ee52e01f52c63160e2a34bea8db1ed5f96
Andrew Sumpter <andrew.sumpter@economicmodeling.com>
2016-09-09 09:58:00 -0700

    exported global header, icon, contentwrap, and logo

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export { default as Select } from './src/components/Select';
 export { default as Option } from './src/components/Option';
 export { default as Button } from './src/components/Button';
+export { default as ContentWrap } from './src/components/ContentWrap';
+export { default as GlobalHeader } from './src/components/GlobalHeader';
+export { default as Icon } from './src/components/Icon';
+export { default as Logo } from './src/components/Logo';
