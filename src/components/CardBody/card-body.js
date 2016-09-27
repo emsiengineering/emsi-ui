3c3053ff1219f73060d68ddc088c33cce4f2de81
Justin Hall <justin.hall@economicmodeling.com>
2016-09-27 12:21:49 -0700

    checking if function definitions cause issues

--- src/components/CardBody/card-body.js
+++ src/components/CardBody/card-body.js
@@ -3,19 +3,12 @@ import React from 'react';
 
 import style from './card-body.css';
 
-type Props = {
-  styles: any,
-  children: any,
-  component?: string,
-  other?: Object
-}
-
-function CardText({ styles, children, component: Component = 'div', ...other }: Props) {
+const CardText = ({ styles, children, component: Component, ...other }) => {
   return (
     <Component styleName='card-body' {...other}>
       {children}
     </Component>
   );
-}
+};
 
 export default CSSModules(CardText, style);
