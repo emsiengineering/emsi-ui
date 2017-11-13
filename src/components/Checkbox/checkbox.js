435dba239895c86a5f7b0a08a6a432d216535269
Andrew Sumpter <thedrew12@gmail.com>
2017-11-13 10:35:23 -0800

    Checkbox/Radio button a11y change (#45)
    * a11y change to checkbox and radio inputs
    
    * build it
    
    * update version

--- src/components/Checkbox/checkbox.js
+++ src/components/Checkbox/checkbox.js
@@ -17,38 +17,51 @@ type Props = {
   checked?: boolean,
   /** add any other props to the component */
   other?: any,
-  children: any
-}
+  children: any,
+  /** unquie id of the label and input, a11y purposes */
+  id?: string
+};
 
 function renderRadio(checked) {
-  return checked ?
-    <Icon name='radioChecked' styleName='selected' size='medium' /> :
-    <Icon name='radio' size='medium' />;
+  return checked ? (
+    <Icon name="radioChecked" styleName="selected" size="medium" />
+  ) : (
+    <Icon name="radio" size="medium" />
+  );
 }
 
 function renderCheckbox(checked) {
-  return checked ?
-    <Icon name='checkboxChecked' size='medium' /> :
-    <Icon name='checkbox' size='medium' />;
+  return checked ? (
+    <Icon name="checkboxChecked" size="medium" />
+  ) : (
+    <Icon name="checkbox" size="medium" />
+  );
 }
 
-function Checkbox({ value, radio, checked, children, ...other }: Props) {
+function Checkbox({ value, radio, checked, children, id, ...other }: Props) {
   const role: string = radio ? 'radio' : 'checkbox';
 
   return (
-    <Label htmlFor={children}>
-      <div styleName='container'>
+    <Label htmlFor={id}>
+      <div styleName="container">
         <div
-          tabIndex='0'
+          tabIndex="0"
           {...other}
           role={role}
           aria-checked={checked}
           styleName={checked ? 'checkbox selected' : 'checkbox'}
         >
           {radio ? renderRadio(checked) : renderCheckbox(checked)}
-          {React.Children.toArray(children).length > 0 && <Text type='caption'>{children}</Text>}
+          {React.Children.toArray(children).length > 0 && (
+            <Text type="caption">{children}</Text>
+          )}
         </div>
-        <input id={children} type={role} style={{ display: 'none' }} defaultValue={value} defaultChecked={checked} />
+        <input
+          id={id}
+          type={role}
+          style={{ display: 'none' }}
+          defaultValue={value}
+          defaultChecked={checked}/>
       </div>
     </Label>
   );
